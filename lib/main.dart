ab56ca905089f5da8c191b8b2b2575788843a5fb
sanayut123 <sanayut4112_@hotmail.com>
2020-08-26 14:49:41 -0700

    routing

--- lib/main.dart
+++ lib/main.dart
@@ -5,60 +5,87 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: MyHomePage(),
+      title: 'app',
+      routes: <String, WidgetBuilder>{
+        '/page1':(BuildContext context)=>Page(), //namePage: 'Page1',color: Colors.red,
+        '/page3':(BuildContext context)=>Page(), //namePage: 'Page2',color: Colors.blue,
+        '/page2':(BuildContext context)=>Page(), //namePage: 'Page3',color: Colors.green,
+      },
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Example navigation routing'),
-      ),
-      body: Container(
-        child: Center(
-          child: RaisedButton(
-            child: Text(
-              'Next Page',
-              style: TextStyle(
-                color: Colors.pink[300],
-                fontSize: 50.0,
-                fontWeight: FontWeight.w800
+        appBar: AppBar(
+          title: Text("Example Routing"),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              RaisedButton(
+                color: Colors.red,
+                child: Text(
+                  "Go to Page 1",
+                  style: TextStyle(
+                    decorationStyle: TextDecorationStyle.dashed
+                  ),
+                ),
+                onPressed: (){
+                  Navigator.pushNamed(context,'/page1');
+                }
               ),
-            ),
-            onPressed: (){
-              Navigator.push(
-                context, 
-                MaterialPageRoute(
-                  builder: (context) =>  NextPage()
-                )
-              );
-            }
+              RaisedButton(
+                color: Colors.red,
+                child: Text(
+                  "Go to Page 2",
+                  style: TextStyle(
+                    decorationStyle: TextDecorationStyle.dashed
+                  ),
+                ),
+                onPressed: (){
+                  Navigator.pushNamed(context,'/page2');
+                }
+              ),
+              RaisedButton(
+                color: Colors.red,
+                child: Text(
+                  "Go to Page 3",
+                  style: TextStyle(
+                    decorationStyle: TextDecorationStyle.dashed
+                  ),
+                ),
+                onPressed: (){
+                  Navigator.pushNamed(context,'/page3');
+                }
+              )
+            ],
           ),
         ),
-        color: Colors.deepPurple[200],
-      ),
-    );
+      );
   }
 }
 
-
-class NextPage extends StatelessWidget {
+class Page extends StatelessWidget {
+  //Page({@required this.namePage,@required this.color});
+  // final String namePage;
+  // final Color color;  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Page 1'),
+        title: Text("namePage"),
       ),
       body: Container(
-        color: Colors.red[300],
+        color: Colors.green,
       ),
     );
   }
